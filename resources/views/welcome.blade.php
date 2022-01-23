e2f8ea56a02b0fda60f161bad843432638194583
Alberto Sanchez Gonzalez <alberto.mu.es@gmail.com>
2022-01-23 12:49:44 +0100

    Build production bundle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html><html lang="en"><head>
   <meta charset="utf-8">
-  <title>Angular</title>
+  <title>WHT test</title>
   <base href="/assets/angular/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<style>@charset "UTF-8";html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}html,body{margin:0;padding:0}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}body{color:var(--text-basic-color);font-family:var(--text-paragraph-font-family);font-size:var(--text-paragraph-font-size);font-weight:var(--text-paragraph-font-weight);line-height:var(--text-paragraph-line-height)}</style><link rel="stylesheet" href="styles.b4edd3e4a1da5dec.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.b4edd3e4a1da5dec.css"></noscript></head>
+<style>@charset "UTF-8";html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}html,body{margin:0;padding:0}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}body{color:var(--text-basic-color);font-family:var(--text-paragraph-font-family);font-size:var(--text-paragraph-font-size);font-weight:var(--text-paragraph-font-weight);line-height:var(--text-paragraph-line-height)}</style><link rel="stylesheet" href="styles.852fafd2fc593f50.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.852fafd2fc593f50.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="runtime.f8b226cf49542a3f.js" type="module"></script><script src="polyfills.4850d75e2df3be65.js" type="module"></script><script src="main.e326ec7ea9815d90.js" type="module"></script>
+<script src="runtime.f8b226cf49542a3f.js" type="module"></script><script src="polyfills.4850d75e2df3be65.js" type="module"></script><script src="main.7a6379cf3bb659db.js" type="module"></script>
 
 </body></html>
